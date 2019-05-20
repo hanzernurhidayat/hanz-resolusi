1afe4dab719bc9cb586c9ed78d8e8d289b069cdd
farhanzhidayat <43440218+farhanzhidayat@users.noreply.github.com>
2019-05-20 16:25:21 +0700

    Menambah password verify

--- login.php
+++ login.php
@@ -5,6 +5,8 @@
   <body>
     <h1>Login disini!</h1>
     <input type="text" name="username"/>
+    <input type="password" name="password"/>
+    <input type="password" name="password2"/>
     <button>Login</button>
   </body>
 </html>
